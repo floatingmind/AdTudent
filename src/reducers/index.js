ad55ba828ea49b0a0bc78d75912fc8a5435bf62b
mehran <mehranahmadi201672@gmail.com>
2019-04-14 23:28:11 +0430

    config redux reducers with combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import advertisementReducer from './advertisementReducer';
+import authReducer from './authReducer';
+import filtersReducer from './filtersReducer';
 
 export default combineReducers({
-    dummy: () => {return {}}
+    auth: authReducer,
+    advertisements: advertisementReducer,
+    filters: filtersReducer
 });
