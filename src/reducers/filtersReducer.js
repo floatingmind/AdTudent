9c82f1153ccd76265b02f5c5d9949824df2a3821
mehran <mehranahmadi201672@gmail.com>
2019-04-14 23:25:05 +0430

    create filters reducer

---
+++ src/reducers/filtersReducer.js
@@ -0,0 +1,14 @@
+import { SET_TEXT_FILTER } from '../actions/types';
+
+const filtersReducerDefaultState = {
+    text: ''
+}
+
+export default (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        case SET_TEXT_FILTER:
+            return { ...state, text: action.text }
+        default:
+            return state;
+    }
+}
