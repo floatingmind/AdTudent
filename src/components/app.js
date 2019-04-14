783b775fe1082db7276946bbe3a7d731ea7ac312
mehran <mehranahmadi201672@gmail.com>
2019-04-14 10:26:02 +0430

    add vazir persian font

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 const App = () => {
-    return <div>App</div>;
+    return <Typography variant="h1">تست 123</Typography>;
 };
 
 export default App;
--- src/theme/MuiTheme.js
+++ src/theme/MuiTheme.js
@@ -1,6 +1,8 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 
+import 'vazir-font/dist/font-face.css';
+
 export default createMuiTheme({
     direction: 'rtl',
     palette: {
