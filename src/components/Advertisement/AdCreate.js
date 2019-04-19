9a8f9edaf18a1025862668d06046ce2f8003a53e
mehran <mehranahmadi201672@gmail.com>
2019-04-19 20:42:45 +0430

    create AdCreate Component for creating ads

--- src/components/Advertisement/AdCreate.js
+++ src/components/Advertisement/AdCreate.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { createAdvertisement } from '../../actions/advertisement';
 
-const AdCreate = () => {
-    return <div>AdCreate</div>
-};
+import AdForm from "./AdForm";
 
-export default AdCreate;
+class AdCreate extends Component {
+  onSubmit = formValues => {
+    this.props.createAdvertisement(formValues);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <AdForm onSubmit={this.onSubmit} mode="createAd"/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default connect( null, {createAdvertisement})(AdCreate);
