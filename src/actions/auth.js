d4fcfa04fed964d6f124d8aa8caaaab894d872e1
mehran <mehranahmadi201672@gmail.com>
2019-04-19 20:48:28 +0430

    use localstorage to persist login status

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,22 @@
-import { SIGN_IN, SIGN_OUT } from './types';
+import { SIGN_IN, SIGN_OUT } from "./types";
 
-export const signIn = userId => ({
+export const signIn = userId => {
+
+  localStorage.setItem("status", true);
+
+  return {
     type: SIGN_IN,
     payload: userId
-  });
-  
-  export const signOut = () => ({
+  };
+
+};
+
+export const signOut = () => {
+
+  localStorage.removeItem("status");
+
+  return {
     type: SIGN_OUT
-  });
+  };
+
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 // Material UI
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -18,21 +19,21 @@ import GlobalStyle from "./theme/StyledTheme";
 // Redux Store
 import store from "./store/configureStore";
 
-import AppRouter from './routers/AppRouter';
+import AppRouter from "./routers/AppRouter";
 
 ReactDOM.render(
   <Provider store={store}>
-    <RTL>
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <ThemeProvider theme={{}}>
-          <React.Fragment>
-            <GlobalStyle />
-            <AppRouter />
-          </React.Fragment>
-        </ThemeProvider>
-      </MuiThemeProvider>
-    </RTL>
+      <RTL>
+        <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+          <ThemeProvider theme={{}}>
+            <React.Fragment>
+              <GlobalStyle />
+              <AppRouter />
+            </React.Fragment>
+          </ThemeProvider>
+        </MuiThemeProvider>
+      </RTL>
   </Provider>,
   document.getElementById("root")
 );
