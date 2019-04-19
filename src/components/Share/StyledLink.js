cbd695c12f0c6709abd4e2640421927ba7c2e0e0
mehran <mehranahmadi201672@gmail.com>
2019-04-19 20:55:58 +0430

    create StyledLink component

---
+++ src/components/Share/StyledLink.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const StyledLink = styled(Link) `
+    text-decoration: none;
+    padding: 0;
+    margin: 0;
+`;
