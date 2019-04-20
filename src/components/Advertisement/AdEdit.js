3ed4bcaf61ef81e34355a3167360a5b8840a9b6a
mehran <mehranahmadi201672@gmail.com>
2019-04-20 22:29:17 +0430

    create AdEdit component

--- src/components/Advertisement/AdEdit.js
+++ src/components/Advertisement/AdEdit.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
 
-const AdEdit = () => {
-    return <div>AdEdit</div>
-};
+import {
+  fetchAdvertisement,
+  editAdvertisement,
+  deleteAdvertisement
+} from "../../actions/advertisement";
 
-export default AdEdit;
+import AdForm from "./AdForm";
+
+class AdEdit extends Component {
+  componentDidMount() {
+    this.props.fetchAdvertisement(this.props.match.params.id);
+  }
+
+  onSubmit = formValues => {
+    this.props.editAdvertisement(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    const { initialValues, mode } = this.props;
+    if (!this.props.advertisement) {
+      return <div>loading</div>;
+    }
+
+    return (
+      <React.Fragment>
+        <AdForm
+          initialValues={_.pick(
+            this.props.advertisement,
+            "surfaceArea",
+            "roomNum",
+            "address"
+          )}
+          onSubmit={this.onSubmit}
+          mode="editAd"
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  advertisement: state.advertisements[ownProps.match.params.id]
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchAdvertisement, editAdvertisement, deleteAdvertisement }
+)(AdEdit);
