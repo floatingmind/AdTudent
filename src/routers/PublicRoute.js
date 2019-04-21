a619a552816969454473728f74fdbdfec93b86e0
mehran <mehranahmadi201672@gmail.com>
2019-04-21 23:00:33 +0430

    add wrapper for public routes

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -2,16 +2,17 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Wrapper } from '../components/Share/PublicRouteWrapper';
 
 const PublicRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     component={props => (
-      <React.Fragment>
+      <Wrapper>
         <Header />
         <Component {...props} />
         <Footer />
-      </React.Fragment>
+      </Wrapper>
     )}
   />
 );
