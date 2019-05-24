8625015a73ff6521ebc21d02882cbd63bb38b2fe
mehran <mehranahmadi201672@gmail.com>
2019-05-24 23:00:19 +0430

    fix test styling for about us component

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import { Typography } from '@material-ui/core';
+import { Wrapper } from './styles';
+
 const AboutUS = () => {
-    return <div style={{ minHeight: '100vh'}}>AboutUS</div>
+    return (<Wrapper>
+        <Typography>این صفحه تستی است</Typography>
+    </Wrapper>)
 };
 
 export default AboutUS;
